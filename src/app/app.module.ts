0c2f4e6dd922f9e8481ed2e154f0d0978c1319e4
Johann <johann.feschet@gmail.com>
2021-10-16 10:31:23 +0200

    init firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,30 @@ import { environment } from '../environments/environment';
 import { FormsModule } from '@angular/forms';
 import { FingerprintAIO } from '@ionic-native/fingerprint-aio/ngx';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyDqO7ZJV0prXdAb6zYOi-BbX6-PR_ltmh8",
+
+  authDomain: "hackatonssg2021.firebaseapp.com",
+
+  databaseURL: "https://hackatonssg2021-default-rtdb.europe-west1.firebasedatabase.app",
+
+  projectId: "hackatonssg2021",
+
+  storageBucket: "hackatonssg2021.appspot.com",
+
+  messagingSenderId: "1061731853608",
+
+  appId: "1:1061731853608:web:f923818760f173a20d1c1d"
+
+};
+
+
 @NgModule({
   imports: [
     BrowserModule,
@@ -24,7 +48,11 @@ import { FingerprintAIO } from '@ionic-native/fingerprint-aio/ngx';
     IonicStorageModule.forRoot(),
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production
-    })
+    }),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    AngularFireStorageModule
   ],
   declarations: [AppComponent],
   providers: [InAppBrowser, SplashScreen, StatusBar, FingerprintAIO],
